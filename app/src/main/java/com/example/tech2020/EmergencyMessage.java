b539ca75c2f2e6d2ed11adeada259a36d001c5fb
smenon1111 <42555379+smenon1111@users.noreply.github.com>
2020-01-19 16:03:19 -0800

    Add Emergency Message and code. Finished Info page Added new files New strings

---
+++ app/src/main/java/com/example/tech2020/EmergencyMessage.java
@@ -0,0 +1,64 @@
+package com.example.tech2020;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class EmergencyMessage extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.emergencymessage);
+        Uri uri;
+        Button sendButton = findViewById(R.id.button);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent smsIntent = new Intent(Intent.ACTION_SENDTO,
+                        Uri.parse("sms:5109538121"));
+                smsIntent.putExtra("sms_body", "Hello");
+                startActivity(smsIntent);
+
+            }
+        });
+
+
+    }
+
+
+    }
