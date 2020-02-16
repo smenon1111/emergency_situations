94bfa0f88051694a351088e59a956c6e770b32b8
smenon1111 <42555379+smenon1111@users.noreply.github.com>
2020-02-16 13:29:11 -0800

    Emergency Message code added and finished

--- app/src/main/java/com/example/tech2020/Home.java
+++ app/src/main/java/com/example/tech2020/Home.java
@@ -2,12 +2,19 @@ package com.example.tech2020;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+
+    public LocationManager locationManager;
+    public Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -19,7 +26,8 @@ public class Home extends AppCompatActivity {
         Button infoButton = findViewById(R.id.buttonInfo);
         Button profileButton = findViewById(R.id.buttonProfile);
         Button playButton = findViewById(R.id.buttonPlay);
-        Button emergencymessageButton = findViewById(R.id.buttonEmergencyMessage);
+        Button emergencyMessageButton = findViewById(R.id.buttonEmergencyMessage);
+        Button iAmSafe = findViewById(R.id.iAmSafe);
 
         //TODO:  LAYA :change openLevels(); to open{insert your page name for levels here}();
         //TODO: ANVIKA: change openProfile(); to open{insert your page name for profile here}();
@@ -27,13 +35,26 @@ public class Home extends AppCompatActivity {
         //TODO: if they are the same as what I labeled them as the just take out the block comments
         //TODO:on your Android Manifest file, under <activity android:name=".Info"></activity add the same line of code, replacing "Info" with your activity name if it's not already there
 
-    emergencymessageButton.setOnClickListener(new View.OnClickListener() {
+        emergencyMessageButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
 
-            openEmergencyMessage();
+            sendEmergencyMessage();
+            Toast messageSentToast = Toast.makeText(getApplicationContext(),"Emergency Message Sent", Toast.LENGTH_SHORT);
+            messageSentToast.show();
         }
-    });
+        });
+
+        iAmSafe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sendSafeMessage();
+                Toast messageSentToast = Toast.makeText(getApplicationContext(),"Safe Message Sent", Toast.LENGTH_SHORT);
+                messageSentToast.show();
+            }
+        });
+
 
 
         levelsButton.setOnClickListener(new View.OnClickListener() {
@@ -92,8 +113,42 @@ public class Home extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void openEmergencyMessage() {
-        Intent intent = new Intent(this, EmergencyMessage.class);
-        startActivity(intent);
+//    public void openEmergencyMessage() {
+//        Intent intent = new Intent(this, EmergencyMessage.class);
+//        startActivity(intent);
+//    }
+
+    public void getEmergencyLocation(){
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    }
+
+    public void sendEmergencyMessage() {
+        getEmergencyLocation();
+
+        SmsManager sms = SmsManager.getDefault();
+        StringBuffer smsBody = new StringBuffer();
+        smsBody.append("Help. I am in trouble. My location is: ");
+        smsBody.append("https://maps.google.com?q=");
+        smsBody.append(location.getLatitude());
+        smsBody.append(",");
+        smsBody.append(location.getLongitude());
+        sms.sendTextMessage("sms:5104493731", null,
+                smsBody.toString(), null, null);
+    }
+
+    public void sendSafeMessage() {
+        getEmergencyLocation();
+
+        SmsManager sms = SmsManager.getDefault();
+        StringBuffer smsBody = new StringBuffer();
+        smsBody.append("I am safe. My location is: ");
+        smsBody.append("https://maps.google.com?q=");
+        smsBody.append(location.getLatitude());
+        smsBody.append(",");
+        smsBody.append(location.getLongitude());
+        sms.sendTextMessage("sms:5104493731", null,
+                smsBody.toString(), null, null);
     }
 }
